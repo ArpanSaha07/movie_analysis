da10c43c3b94dc4b1a58f52c3a0aca4ce4523eb1
Danning <81199737+Shark1328@users.noreply.github.com>
2023-12-10 09:28:48 -0500

    Add files via upload

--- scripts/tfidf.py
+++ scripts/tfidf.py
@@ -2,7 +2,7 @@ import argparse
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-def compute_top_words(input_file, output_file):
+def compute_top_words_and_tfidf(input_file, output_file):
     # Load the annotated data from the CSV file
     df = pd.read_csv(input_file)
 
@@ -19,7 +19,7 @@ def compute_top_words(input_file, output_file):
     # Get the feature names (words) from the TF-IDF matrix
     feature_names = tfidf_vectorizer.get_feature_names_out()
 
-    # Create a DataFrame to store the top 10 words for each category
+    # Create a DataFrame to store the top 10 words and TF-IDF scores for each category
     top_words_df = pd.DataFrame(index=range(10))
 
     # Iterate through each unique category in 'Coded_Topics'
@@ -39,17 +39,22 @@ def compute_top_words(input_file, output_file):
             # Get the actual words from the feature names
             top_words = [feature_names[idx] for idx in top_word_indices]
 
-            # Add the top words to the DataFrame with the category as the column name
-            top_words_df[category] = top_words
+            # Get the TF-IDF scores for the top words
+            top_tfidf_scores = avg_tfidf_scores[top_word_indices]
 
-    # Save the top words DataFrame to a CSV file
+            # Add the top words and TF-IDF scores to the DataFrame with the category as the column name
+            top_words_df[f'{category}_words'] = top_words
+            top_words_df[f'{category}_tfidf'] = top_tfidf_scores
+
+    # Save the top words and TF-IDF scores DataFrame to a CSV file
     top_words_df.to_csv(output_file, index=False)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Compute top words using TF-IDF scores.")
+    parser = argparse.ArgumentParser(description="Compute top words and TF-IDF scores using TF-IDF.")
     parser.add_argument("-i", "--input", help="Input CSV file path", required=True)
     parser.add_argument("-o", "--output", help="Output CSV file path", required=True)
     args = parser.parse_args()
 
     # Call the function with provided input and output file paths
-    compute_top_words(args.input, args.output)
+    compute_top_words_and_tfidf(args.input, args.output)
+
